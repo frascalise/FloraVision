f8fccd9fff6dfe98dcfad39f980828bdef2f677a
frascalise <scalise92111@gmail.com>
2025-07-11 18:31:06 +0200

    Updated alexnet_train

--- alexnet/train_alexnet.py
+++ alexnet/train_alexnet.py
@@ -1,26 +1,23 @@
-from torchvision import transforms, datasets
-import torchvision.models as models
 import torch
+import torch.nn as nn
+import torchvision.models as models
+import yaml
+from torchvision.models import AlexNet_Weights
+from torchvision import datasets
 from torch.optim import SGD 
 from torch.utils.data import DataLoader
-import torch.nn as nn
-import yaml
 
 
 def getDataset():
-    transform = transforms.Compose([
-        transforms.Resize(227), # Resize to 227x227 for AlexNet (standard AlexNet's input size)
-        transforms.ToTensor(),  # Convert PIL images to PyTorch tensors
-        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225]) # ImageNet mean and std
-    ])
+    # Default transforms for pretrained AlexNet 
+    # (https://docs.pytorch.org/vision/main/models/generated/torchvision.models.alexnet.html) 
+    transform = AlexNet_Weights.IMAGENET1K_V1.transforms()
 
     # Load dataset path from ./params.yaml
     with open('params.yaml', 'r') as file:
         params = yaml.safe_load(file)
         dataset_path = params['dataset_path']
     
-    # ImageFolder expects a directory structure like:
-    # dataset_path/train/class_name/xxx.png
     train = datasets.ImageFolder(
         root=f"{dataset_path}/train",
         transform=transform
@@ -33,55 +30,42 @@ def getDataset():
     return train, valid
 
 
-def alexnet_train(model, train_dataset, valid_dataset, num_epochs=50, patience=5):
-    # Set the device to GPU if available, otherwise CPU and move the model to that device
+def alexnet_train(model, train_dataset, valid_dataset, num_epochs=50, patience=3):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
-    # DataLoader divides the dataset into batches for training and validation
-    # It's efficient because it doesn't load the entire dataset into memory at once
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True) 
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
     valid_loader = DataLoader(valid_dataset, batch_size=32, shuffle=False)
 
-    # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
-    optimizer = SGD(model.parameters(), lr=0.01, momentum=0.9, weight_decay=0.0005) # Stochastic Gradient Descent optimizer
+    optimizer = SGD(model.parameters(), lr=0.001, momentum=0.9, weight_decay=0.0005) # Stochastic Gradient Descent optimizer
 
-    # Early stopping parameters
-    best_valid_loss = float('inf') # Initialize best validation loss to infinity so that any valid loss will be lower
+    best_valid_loss = float('inf')
     epochs_no_improve = 0
 
     for epoch in range(num_epochs):
         print(f"\nEpoch {epoch+1}/{num_epochs}", flush=True)
 
-        # Model set to training mode (dropout enabled -> some neurons will be randomly dropped)
+        # Training phase
         model.train()
         running_loss = 0.0
 
         for train_inputs, train_labels in train_loader:
-            # Move input batch and label batch to the device
             inputs, labels = train_inputs.to(device), train_labels.to(device)
 
-            optimizer.zero_grad()   # Set gradients to zero (prevent accumulation from previous iterations)
+            optimizer.zero_grad()
             outputs = model(inputs)
-            loss = criterion(outputs, labels)   # Calculate loss (in this case, CrossEntropyLoss)
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
 
-            loss.backward()     # Computes the loss gradient with respect to the model parameters 
-                                # (it fill the .grad attributes of the model parameters)
-
-            optimizer.step()    # Applies the gradients (calulated in loss.backward()) to the model parameters
-
-            # Accumulate loss for the batch
-            # inputs.size(0) gives the batch size, so we multiply batch avg. loss by batch size to get total loss for the batch
             running_loss += loss.item() * inputs.size(0)
 
         epoch_loss = running_loss / len(train_dataset)
         
-        # Model set to evaluation mode (dropout disabled -> all neurons are used)
+        # Validation phase
         model.eval()
         valid_loss = 0.0
-
-        # Disable gradient calculation for validation to save memory and computation
         with torch.no_grad():
             for val_inputs, val_labels in valid_loader:
                 inputs, labels = val_inputs.to(device), val_labels.to(device)
@@ -94,8 +78,6 @@ def alexnet_train(model, train_dataset, valid_dataset, num_epochs=50, patience=5
         print(f"Train Loss: {epoch_loss:.4f}, Valid Loss: {valid_loss:.4f}", flush=True)
 
         # Early stopping
-        # If validation loss improves (for example, from 0.18 to 0.17), save the model
-        # If validation loss does not improve for 'patience' epochs, stop training
         if valid_loss < best_valid_loss:
             best_valid_loss = valid_loss
             epochs_no_improve = 0
@@ -108,7 +90,9 @@ def alexnet_train(model, train_dataset, valid_dataset, num_epochs=50, patience=5
                 print("Early stopping triggered", flush=True)
                 break
     
+    model.load_state_dict(torch.load('alexnet.pth'))
     print("\n\nTraining complete. Best model saved as 'alexnet.pth'", flush=True)
+    return model
 
 
 if __name__ == "__main__":
@@ -117,9 +101,9 @@ if __name__ == "__main__":
     train, valid = getDataset()
 
     print("Creating AlexNet model...", flush=True)
-    alexnet = models.alexnet(pretrained=True)
+    alexnet = models.alexnet(weights='IMAGENET1K_V1')
     num_classes = len(train.classes)
     alexnet.classifier[6] = nn.Linear(4096, num_classes)
 
     print("Training AlexNet model...", flush=True)
-    alexnet_train(alexnet, train, valid)
+    alexnet = alexnet_train(alexnet, train, valid)
